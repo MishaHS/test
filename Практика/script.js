9a693ca3d9ce700c68c558e4bd0e7931027ae6ce
Misha <android4427@gmail.com>
2019-04-30 22:16:46 +0600

    это первый комит

---
+++ Практика/script.js
@@ -0,0 +1,11 @@
+'use strick';
+/*  let f =confirm("ты тут?");
+if (f==true){
+    alert("calam");
+}
+else {
+    alert("c23alam");
+}  */
+let answer = prompt("сколько лет?", "");
+
+console.log (typeof(answer));
